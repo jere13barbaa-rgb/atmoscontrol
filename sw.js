52d7da0fb1cde88143f87ccf7325389304c592f4
jere13barbaa-rgb <jere13barbaa@gmail.com>
2026-07-06 18:53:29 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'atmoscontrol-v1';
+const assets = [
+  'index.html',
+  'manifest.json',
+  'image_3.png'
+];
+
+// Instalar el Service Worker y guardar en caché los archivos base
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Interceptar las peticiones para que cargue rápido
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
